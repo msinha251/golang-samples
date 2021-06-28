e9d959baae5ff5b3954dc56d65f635739e337f35
Mahesh Sinha <Mahesh.Sinha@externals.adidas.com>
2021-06-28 23:24:40 +0200

    Adding go-crud with mongo

---
+++ 4-go-crud-mongo/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Article struct {
+	Id      string `bson:id`
+	Title   string `bson:title`
+	Content string `bson:content`
+}
+
+// ************ MONGO CONNECTION **************
+
+func mongoConnect(ctx context.Context) (*mongo.Client, error) {
+	var mongoConnectionError error
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		mongoConnectionError = err
+		log.Fatal(err)
+	}
+	return client, mongoConnectionError
+}
+
+// *****************************************
+
+// ************ API ENDPOINTS **************
+
+func getDatabases(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongoConnect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, _ := client.ListDatabaseNames(ctx, bson.D{})
+	fmt.Fprintf(w, strconv.Itoa(len(databases)))
+}
+
+func addArticle(w http.ResponseWriter, r *http.Request) {
+	var article Article
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	// ********* mongo **********
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongoConnect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// *************************
+
+	json.Unmarshal(reqBody, &article)
+	// article := Article{
+	// 	Id:      "1",
+	// 	Title:   "Title 1",
+	// 	Content: "my articles content",
+	// }
+
+	client.Database("go-crud").Collection("go-crud").InsertOne(ctx, article)
+	fmt.Fprintf(w, "Article added.")
+}
+
+func getArticles(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, _ := mongoConnect(ctx)
+	articles, _ := client.Database("go-crud").Collection("go-crud").Find(ctx, bson.D{})
+	var allArticles []bson.M
+	fmt.Println(articles.All(ctx, &allArticles))
+
+	results, _ := json.Marshal(articles)
+	fmt.Fprintf(w, string(results))
+
+}
+
+func handlers() {
+	router := mux.NewRouter()
+	router.HandleFunc("/getdatabases", getDatabases)
+	router.HandleFunc("/addarticle", addArticle).Methods("POST")
+	router.HandleFunc("/getarticles", getArticles)
+	http.ListenAndServe(":8080", router)
+}
+
+// *****************************************
+
+func main() {
+	handlers()
+}
